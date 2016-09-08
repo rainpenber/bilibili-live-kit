39a9e0cfe2acb09530b6065166a8db41cfac916c
Septs <cao111210@gmail.com>
2016-09-09 02:59:28 +0800

    refactor CAPTCHA

--- bilibili_live_kit/utils/captcha.py
+++ bilibili_live_kit/utils/captcha.py
@@ -1,8 +1,41 @@
+from functools import lru_cache
+
 from PIL import Image
 
-from .captcha_sample import payload as samples
-
 __ALL__ = ['get_captcha']
+
+
+@lru_cache(maxsize=1)
+def get_samples():
+    import base64
+    import gzip
+    import json
+    payload = b'''
+    H4sIAJug0VcC/+2YbQ7CIAyGr7Lsr5q0futVjCcx3l3RbbQM1gFjm0P+iE8Io/SltDzKQ3ktbiUa
+    rVwX8Qx0m5zpBrG2kelHZcS28VnIXt3fcP+RF7Wl8Qu1rw8kawuAVBn+kOnKG1as+vGF9YxhsFlP
+    EMQ+jvaEhrSZ34eDSnyXr/gGPv+TnsM5nOtGLGnj4uxj23ZR8up2i7aNIxvjR7L+lsCaJdnCeCcT
+    or0tMPNoKzGeVGjG0wxjGETL67z46GVeOSSymPIiO2C4lMumPc65f3qG7CK4ktdxCfKaZYGQfYKg
+    5LXhdSUbx/+o0ViXCfrmQaGLQhetXXne3++qDV2RuiuucAqH6YqH1ItXO7jLOrvDdiaH7awNLVmb
+    bZx1PnsGmcvDyCn1u5uLGdcgdrLOAgBcLu3JQGYYyUAsMrwYBBUoTgZBhYxY3Ch5wf/p488SRa/n
+    CzyRsY42GAAA
+    '''
+    return json.loads(gzip.decompress(base64.decodebytes(payload)).decode())
+
+
+def get_symbol(code, threshold):
+    symbol_key = '?'
+    symbol_score = 0
+    for key, value in get_samples().items():
+        value = '-'.join(value)
+        if len(value) != len(code):
+            continue
+        score = len(list(filter(lambda offset: value[offset] == code[offset], range(len(value)))))
+        score /= len(value)
+        if score < symbol_score and score < threshold:
+            continue
+        symbol_key = key
+        symbol_score = score
+    return symbol_key
 
 
 def trim_y(image):
@@ -31,22 +64,6 @@ def image_to_ascii_image(image):
             yield '-'
 
     return ''.join(handle()).strip('-')
-
-
-def get_symbol(code, threshold):
-    symbol_key = '?'
-    symbol_score = 0
-    for key, value in samples.items():
-        value = '-'.join(value)
-        if len(value) != len(code):
-            continue
-        score = len(list(filter(lambda offset: value[offset] == code[offset], range(len(value)))))
-        score /= len(value)
-        if score < symbol_score and score < threshold:
-            continue
-        symbol_key = key
-        symbol_score = score
-    return symbol_key
 
 
 def get_sub_image(image):
